ce0c0f84d1ab785af2cbf6a2d5df4015702f4f54
BenPruden97 <benpruden34@yahoo.com>
2021-06-21 21:07:29 +0000

    Added the High Score section as well as the styling and media queries for responsive design

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,16 @@ document.getElementById("close-how-to").addEventListener('click', function() {
     document.querySelector(".how-to").style.display = "none";
 });
 
+/* High Score button to open and close for high score pop up */
+
+document.getElementById("open-high-score").addEventListener('click', function() {
+    document.querySelector(".high-score-popup").style.display = "block";
+});
+
+document.getElementById("close-high-score").addEventListener('click', function() {
+    document.querySelector(".high-score-popup").style.display = "none";
+});
+
 
 /* Contact button to open and close the contact form */
 
