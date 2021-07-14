a478bbd98302509cf0394ac83ff5d24dc404d5a9
BenPruden97 <benpruden34@yahoo.com>
2021-07-14 15:47:52 +0000

    Created an extra button for highscores.html file for navigating back to the main menu as well as styling the button

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,6 @@ document.getElementById("open-how-to").addEventListener('click', function() {
 
 document.getElementById("close-how-to").addEventListener('click', function() {
     document.querySelector(".how-to").style.display = "none";
-}); 
-
-// High Score button to open and close for high score pop up //
-
-document.getElementById("open-high-score").addEventListener('click', function() {
-    document.querySelector(".high-score-popup").style.display = "block";
-});
-
-document.getElementById("close-high-score").addEventListener('click', function() {
-    document.querySelector(".high-score-popup").style.display = "none";
 });
 
 // Contact button to open and close the contact form //
@@ -27,4 +17,5 @@ document.getElementById("open-contact-form").addEventListener('click', function(
 document.getElementById("close-contact-form").addEventListener('click', function() {
     document.querySelector(".contact-form").style.display = "none";
 });
+
 
